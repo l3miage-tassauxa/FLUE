237dcec282e8046da84247b4f085d658eff9213b
tassauxa <aurelien.tassaux@etu.univ-grenoble-alpes.fr>
2025-06-06 13:31:22 +0200

    1et jet installation des librairies

--- flue/ajout_Modele.sh
+++ flue/ajout_Modele.sh
@@ -1,20 +1,41 @@
 #!/usr/bin/env bash
 # Aurélien Tassaux
 
-# Check if the first argument is provided
+#Macros
+DATA_DIR=./Data
+MODEL_DIR=./Model
+MODEL_PATH=$MODEL_DIR/model.pth
+
+# Check isi le premier argument est fourni
 if [ -z "$1" ]; then
         echo "Veuiller spécifier une tache."
         exit 1
 fi
-# Check if the second argument is provided
+# Check si le deuxième argument est fourni
 if [ -z "$2" ]; then
         echo "Veuiller spécifier si les librairies doivent être installées (true/false)."
         exit 1
 fi
 
-# Install required libraries
+# Installe les libraries requises
 if [ $2 == true ]; then
-    ./install_libraries.sh
+    # Check si on est dans le dossier flue
+    if [ "$(basename "$PWD")" != "flue" ]; then
+        echo "Veuillez positionner le terminal dans le dossier FLUE/flue."
+        exit 1
+    fi
+    echo "Installing required libraries..."
+    pip install -r requirements.txt
+    cd ..
+    cd tools
+    git clone https://github.com/attardi/wikiextractor.git
+    git clone https://github.com/moses-smt/mosesdecoder.git
+    git clone https://github.com/glample/fastBPE.git
+    cd fastBPE
+    g++ -std=c++11 -pthread -O3 fastBPE/main.cc -IfastBPE -o fast
+    cd ..
+    cd ..
+    cd flue
     echo "Libraries installed."
 else
     echo "Skipping library installation."
@@ -24,23 +45,30 @@ fi
 case $1 in
     cls)
     echo "Getting CLS data..."
-        ./get-data-cls.sh ./Data
+        ./get-data-cls.sh $DATA_DIR
         echo "Preparing CLS data..."
-        ./flue/prepare-data-cls.sh ./Data ./Model/model.pth false
+        ./flue/prepare-data-cls.sh $DATA_DIR $MODEL_PATH false
         ;;
 
     pawsx)
         echo "Getting PAWSX data..."
-        ./get-data-xnli.sh ./Data
+        ./get-data-xnli.sh $DATA_DIR
         echo "Preparing PAWSX data..."
-        ./flue/prepare-data-pawsx.sh ./Data ./Model/model.pth false
+        ./flue/prepare-data-pawsx.sh $DATA_DIR $MODEL_PATH false
         ;;
 
     xnli)
         echo "Getting XNLI data..."
-        ./get-data-xnli.sh ./Data
+        ./get-data-xnli.sh $DATA_DIR
         echo "Preparing XNLI data..."
-        ./flue/prepare-data-xnli.sh ./Data ./Model/model.pth false
+        ./flue/prepare-data-xnli.sh $DATA_DIR $MODEL_PATH false
+        ;;
+    vsd)
+        echo "Getting VSD data..."
+        FSE_DIR=./Data/FSE-1.1-191210
+        python prepare_data.py --data $FSE_DIR --output $DATA_DIR
+        echo "Preparing VSD data..."
+        ./flue/prepare-data-vsd.sh $DATA_DIR $MODEL_PATH false
         ;;
     *)
         echo "Veuiller spécifier une tache valide."
