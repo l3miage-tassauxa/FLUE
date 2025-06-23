e61978820b9f477be9215e55a9fbf2669a31a28e
tassauxa <aurelien.tassaux@etu.univ-grenoble-alpes.fr>
2025-06-23 14:16:08 +0200

    Refactor evaluation scripts and configuration files for CLS and XNLI tasks, updating model paths and adding support for Hugging Face integration.

--- flue/evaluation_auto.sh
+++ flue/evaluation_auto.sh
@@ -53,30 +53,9 @@ case $1 in
             tar -xvf ./flue/data/cls/raw/cls-acl10-unprocessed.tar.gz -C ./flue/data/cls/raw/
             echo "Data unzipped."
         fi
-        echo "Preparing CLS Books data..."
-        ./flue/prepare-data-cls-origin.sh $DATA_DIR/cls $MODEL_PATH/flaubert_base_cased_xlm true
-        echo "Running CLS books evaluation..."
-        config='flue/examples/cls_books_lr5e6_xlm_base_cased.cfg'
-        source $config
-        python flue/flue_xnli.py --exp_name $exp_name \
-                        --exp_id $exp_id \
-                        --dump_path $dump_path  \
-                        --model_path $model_path  \
-                        --data_path $data_path  \
-                        --dropout $dropout \
-                        --transfer_tasks $transfer_tasks \
-                        --optimizer_e adam,lr=$lre \
-                        --optimizer_p adam,lr=$lrp \
-                        --finetune_layers $finetune_layers \
-                        --batch_size $batch_size \
-                        --n_epochs $num_epochs \
-                        --epoch_size $epoch_size \
-                        --max_len $max_len \
-                        --max_vocab $max_vocab
-        echo "Calculating accuracy from task 1 predictions..."
-        python flue/accuracy_from_task1.py --logits_file ./experiments/Flaubert/cls_booksxlm_base_cased/bs_8_dropout_0.1_ep_30_lre_5e6_lrp_5e6/test.pred.29 --labels_file ./flue/data/cls/processed/books/test.label
-        echo "Preparing CLS DVD data..."
-        ./flue/prepare-data-cls-origin.sh $DATA_DIR/cls $MODEL_PATH/flaubert_base_cased_xlm true
+c
+        echo "Preparing CLS dvd data..."
+        ./flue/prepare-data-cls-origin.sh $DATA_DIR/cls $MODEL_PATH/flaubert_base_cased_xlm_dvd true
         echo "Running CLS DVD evaluation..."
         config='flue/examples/cls_DVD_lr5e6_xlm_base_cased.cfg'
         source $config
@@ -97,8 +76,8 @@ case $1 in
                         --max_vocab $max_vocab
         echo "Calculating accuracy from task 1 predictions..."
         python flue/accuracy_from_task1.py --logits_file ./experiments/Flaubert/cls_dvdxlm_base_cased/bs_8_dropout_0.1_ep_30_lre_5e6_lrp_5e6/test.pred.29 --labels_file ./flue/data/cls/processed/dvd/test.label
-        echo "Preparing CLS Music data..."
-        ./flue/prepare-data-cls-origin.sh $DATA_DIR/cls $MODEL_PATH/flaubert_base_cased_xlm true
+        echo "Preparing CLS music data..."
+        ./flue/prepare-data-cls-origin.sh $DATA_DIR/cls $MODEL_PATH/flaubert_base_cased_xlm_music true
         echo "Running CLS music evaluation..."
         config='flue/examples/cls_music_lr5e6_xlm_base_cased.cfg'
         source $config
@@ -120,7 +99,53 @@ case $1 in
         echo "Calculating accuracy from task 1 predictions..."
         python flue/accuracy_from_task1.py --logits_file "./experiments/Flaubert/cls_musicxlm_base_cased/cls_musicxlm_base_cased/bs_8_dropout_0.1_ep_30_lre_5e6_lrp_5e6/test.pred.29" --labels_file "./flue/data/cls/processed/music/test.label"
         ;;
-
+    cls-HF)
+        if [ $2 == true ]; then
+            echo "Installing required libraries..."
+            pip install -r ./libraries/XLM-requirements.txt
+            echo "Libraries installed."
+        else
+            echo "Skipping library installation."
+        fi
+        echo "Ajout des droits d'exécution aux scripts..."
+        chmod +x ./flue/prepare-data-cls-origin.sh ./flue/extract_split_cls.py ./flue/binarize.py
+        chmod +x ./flue/pretrained_models/flaubert_small_cased_xlm/*
+        echo "Getting CLS data..."
+        if [ ! -f "$DATA_DIR/cls/raw/cls-acl10-unprocessed.tar.gz" ]; then
+            echo "You must make a demand for the data at https://zenodo.org/record/3251672"
+            echo "and place the file in $DATA_DIR/cls/raw/cls-acl10-unprocessed.tar"
+            exit 1
+        else
+            echo "Unzipping data..."
+            tar -xvf ./flue/data/cls/raw/cls-acl10-unprocessed.tar.gz -C ./flue/data/cls/raw/
+            echo "Data unzipped."
+        fi
+        echo "Preparing CLS books data..."
+        python flue/extract_split_cls.py --indir $DATA_DIR/cls/raw/cls-acl10-unprocessed \
+                                 --outdir $DATA_DIR/cls/processed \
+                                 --do_lower true \
+                                 --use_hugging_face true
+        echo "Running CLS books evaluation..."
+        config='flue/examples/cls_books_lr5e6_hf_base_uncased.cfg'
+        source $config
+        python ~/transformers/examples/run_glue.py \
+                                        --data_dir $data_dir \
+                                        --model_type flaubert \
+                                        --model_name_or_path $model_name_or_path \
+                                        --task_name $task_name \
+                                        --output_dir $output_dir \
+                                        --max_seq_length 512 \
+                                        --do_train \
+                                        --do_eval \
+                                        --learning_rate $lr \
+                                        --num_train_epochs $epochs \
+                                        --save_steps $save_steps \
+                                        --fp16 \
+                                        --fp16_opt_level O1 \
+                                        |& tee output.log
+        echo "Calculating accuracy from task 1 predictions..."
+        python flue/accuracy_from_task1.py --logits_file ./experiments/Flaubert/cls_booksxlm_base_cased/bs_8_dropout_0.1_ep_30_lre_5e6_lrp_5e6/test.pred.29 --labels_file ./flue/data/cls/processed/books/test.label
+        ;;
     pawsx)
         if [ $2 == true ]; then
             echo "Installing required libraries..."
@@ -183,15 +208,16 @@ case $1 in
         echo "End of XNLI evaluation."
         ;;
     xnli-HF)
-        if [ $2 == true ]; then
+                if [ $2 == true ]; then
             echo "Installing required libraries..."
-            pip install -r ./libraries/hg-requirements.txt
+            pip install -r ./libraries/XLM-requirements.txt
             echo "Libraries installed."
         else
             echo "Skipping library installation."
         fi
         echo "Ajout des droits d'exécution aux scripts..."
-        chmod +x ./flue/get-data-xnli.sh ./flue/extract_xnli.py ./flue/flue_xnli.py
+        chmod +x ./flue/get-data-xnli.sh ./flue/prepare-data-xnli.sh ./flue/flue_xnli.py ./flue/extract_xnli.py
+        chmod +x ./flue/pretrained_models/flaubert_base_cased_xlm/*
         echo "Getting XNLI data..."
         ./flue/get-data-xnli.sh $DATA_DIR/xnli
         echo "Preparing XNLI data..."
